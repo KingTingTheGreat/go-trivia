18cc60cd04695ca2f9e89d9c7fe320178c3319e5
KingTingTheGreat <jting@bu.edu>
2024-03-07 13:33:07 -0500

    major refactoring. may have introduced some bugs

--- main.go
+++ main.go
@@ -33,10 +33,49 @@ func main() {
 	questionNumber := 0
 	password := configs.Password()
 
+	parseJSON := func(c echo.Context) (string, bool, int64, error) {
+		var realPlayer string
+		var correctPassword bool
+		var amountInt int64
+
+		json_map := make(map[string]interface{})
+		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+		if err != nil {
+			fmt.Println("Error decoding json")
+			return realPlayer, correctPassword, amountInt, err
+		}
+
+		// name
+		realPlayer = json_map["name"].(string)
+
+		// password
+		inputPassword, ok := json_map["password"].(string)
+		if ok {
+			correctPassword = inputPassword == password
+		}
+
+		// amount
+		amount, ok := json_map["amount"].(string)
+		if ok {
+			amountInt, err = strconv.ParseInt(amount, 10, 64)
+			if err != nil {
+				fmt.Println("Error parsing amount")
+				return realPlayer, correctPassword, amountInt, err
+			}
+		}
+
+		return realPlayer, correctPassword, amountInt, nil
+	}
+	cleanName := func(name string) string {
+		return strings.ToLower(strings.TrimSpace(name))
+	}
+
 	e := echo.New()
 
+	// static files
 	e.Static("/", "public")
 
+	// pages
 	e.GET("/", controllers.Home)
 
 	e.GET("/play/:name", controllers.Play)
@@ -59,14 +98,16 @@ func main() {
 		for playerName, _ := range playerScores {
 			players = append(players, playerNames[playerName])
 		}
-		// sort alphabetically
-		sort.Strings(players)
+		// sort alphabetically without case sensitivity
+		sort.Slice(players, func(i, j int) bool {
+			return cleanName(players[i]) < cleanName(players[j])
+		})
 		return c.JSON(200, players)
 	})
 
+	// player actions
 	e.POST("/check-in", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+		realPlayer, _, _, err := parseJSON(c)
 		if err != nil {
 			fmt.Println("Error decoding json")
 			return err
@@ -74,16 +115,14 @@ func main() {
 		Lock.Lock()
 		defer Lock.Unlock()
 
-		realPlayer := json_map["name"].(string)
-		playerName := strings.TrimSpace(realPlayer)
-		playerName = strings.ToLower(playerName)
+		playerName := cleanName(realPlayer)
 
-		// set player name to real name
+		// set player name to real name if not exists
 		if _, ok := playerNames[playerName]; !ok {
 			playerNames[playerName] = realPlayer
 		}
 
-		// set player score to 0 if exists
+		// set player score to 0 if not exists
 		if _, ok := playerScores[playerName]; !ok {
 			playerScores[playerName] = 0
 			playerCorrect[playerName] = make([]string, 0)
@@ -93,8 +132,7 @@ func main() {
 		return c.String(200, fmt.Sprintf("%v", questionNumber))
 	})
 	e.POST("/buzz", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+		realPlayer, _, _, err := parseJSON(c)
 		if err != nil {
 			fmt.Println("Error decoding json")
 			return err
@@ -103,13 +141,16 @@ func main() {
 		Lock.Lock()
 		defer Lock.Unlock()
 
-		realPlayer := json_map["name"].(string)
-		playerName := strings.TrimSpace(realPlayer)
-		playerName = strings.ToLower(playerName)
+		playerName := cleanName(realPlayer)
 
 		// player buzzed in
 		if _, ok := playerTimes[playerName]; !ok {
 			playerTimes[playerName] = time.Now()
+		}
+
+		// store real name if not exists
+		if _, ok := playerNames[playerName]; !ok {
+			playerNames[playerName] = realPlayer
 		}
 
 		// give player a score if this is their first buzz
@@ -119,32 +160,22 @@ func main() {
 			lastUpdate[playerName] = time.Now()
 		}
 
-		// store real name if not exists
-		if _, ok := playerNames[playerName]; !ok {
-			playerNames[playerName] = realPlayer
-		}
-
 		return c.String(200, fmt.Sprintf("%v", questionNumber))
 	})
 
+	// host actions
 	e.POST("/clear", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+		_, correctPassword, _, err := parseJSON(c)
 		if err != nil {
 			fmt.Println("Error decoding json")
-			return c.String(400, "Bad Request: Invalid JSON")
+			return err
 		}
 
 		Lock.Lock()
 		defer Lock.Unlock()
 
 		// verify password
-		inputPassword, ok := json_map["password"].(string)
-		if !ok {
-			fmt.Println("No password")
-			return c.String(400, "Bad Request: No password")
-		}
-		if inputPassword != password {
+		if !correctPassword {
 			fmt.Println("Unauthorized")
 			return c.String(401, "Unauthorized")
 		}
@@ -153,9 +184,8 @@ func main() {
 		playerTimes = make(map[string]time.Time)
 		return c.String(200, "Clear")
 	})
-	e.POST("/next", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+	changeQuestion := func(c echo.Context, inc bool) error {
+		_, correctPassword, _, err := parseJSON(c)
 		if err != nil {
 			fmt.Println("Error decoding json")
 			return c.String(400, "Bad Request: Invalid JSON")
@@ -165,107 +195,29 @@ func main() {
 		defer Lock.Unlock()
 
 		// verify password
-		inputPassword, ok := json_map["password"].(string)
-		if !ok {
-			fmt.Println("No password")
-			return c.String(400, "Bad Request: No password")
-		}
-		if inputPassword != password {
+		if !correctPassword {
 			fmt.Println("Unauthorized")
 			return c.String(401, "Unauthorized")
 		}
 
-		// clear all player buzz in times and increment question number
-		questionNumber += 1
+		// increment or decrement question number
+		if inc {
+			questionNumber += 1
+		} else {
+			questionNumber -= 1
+		}
+		// reset all player buzz in times
 		playerTimes = make(map[string]time.Time)
 		return c.String(200, fmt.Sprintf("%v", questionNumber))
+	}
+	e.POST("/next", func(c echo.Context) error {
+		return changeQuestion(c, true)
 	})
 	e.POST("/prev", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
-		if err != nil {
-			fmt.Println("Error decoding json")
-			return c.String(400, "Bad Request: Invalid JSON")
-		}
-
-		Lock.Lock()
-		defer Lock.Unlock()
-
-		// verify password
-		inputPassword, ok := json_map["password"].(string)
-		if !ok {
-			fmt.Println("No password")
-			return c.String(400, "Bad Request: No password")
-		}
-		if inputPassword != password {
-			fmt.Println("Unauthorized")
-			return c.String(401, "Unauthorized")
-		}
-		if questionNumber == 0 {
-			return c.String(200, fmt.Sprintf("%v", questionNumber))
-		}
-
-		// clear all player buzz in times and increment question number
-		questionNumber -= 1
-		playerTimes = make(map[string]time.Time)
-		return c.String(200, fmt.Sprintf("%v", questionNumber))
+		return changeQuestion(c, false)
 	})
-
-	e.POST("/leaderboard", func(c echo.Context) error {
-		Lock.Lock()
-		defer Lock.Unlock()
-
-		// list of all players and their scores
-		playerWithScores := make([][]string, 0)
-		for playerName, score := range playerScores {
-			playerWithScores = append(playerWithScores, []string{playerNames[playerName], fmt.Sprintf("%d", score)})
-		}
-
-		// sort players by score, then by last update time
-		sort.Slice(playerWithScores, func(i, j int) bool {
-			a := playerScores[playerWithScores[i][0]]
-			b := playerScores[playerWithScores[j][0]]
-			if a == b {
-				return lastUpdate[playerWithScores[i][0]].Before(lastUpdate[playerWithScores[j][0]])
-			}
-			return a > b
-		})
-
-		return c.JSON(200, playerWithScores)
-	})
-
-	e.POST("/buzzed-in", func(c echo.Context) error {
-		Lock.Lock()
-		defer Lock.Unlock()
-
-		// list all players and their buzz in times, in order of buzz in
-		players := make([][]string, 0)
-		for playerName, _ := range playerTimes {
-			players = append(players, []string{playerNames[playerName], playerTimes[playerName].Format("03:04:05.000 PM")})
-		}
-		sort.Slice(players, func(i, j int) bool {
-			return playerTimes[players[i][0]].Before(playerTimes[players[j][0]])
-		})
-
-		return c.JSON(200, players)
-	})
-
-	e.POST("/stats", func(c echo.Context) error {
-		Lock.Lock()
-		defer Lock.Unlock()
-
-		// list all players and their scores and correct answers
-		players := make([][]string, 0)
-		for playerName, score := range playerScores {
-			players = append(players, []string{playerNames[playerName], fmt.Sprintf("%d", score), strings.Trim(strings.Join(playerCorrect[playerName], ","), "[]")})
-		}
-
-		return c.JSON(200, players)
-	})
-
 	e.PUT("/update-score", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+		realPlayer, correctPassword, amountInt, err := parseJSON(c)
 		if err != nil {
 			fmt.Println("Error decoding json")
 			return c.String(400, "Bad Request: Invalid JSON")
@@ -275,34 +227,13 @@ func main() {
 		defer Lock.Unlock()
 
 		// verify password
-		inputPassword, ok := json_map["password"].(string)
-		if !ok {
-			fmt.Println("No password")
-			return c.String(400, "Bad Request: No password")
-		}
-		if inputPassword != password {
+		if !correctPassword {
 			fmt.Println("Unauthorized")
 			return c.String(401, "Unauthorized")
 		}
 		// verify playername and amount
-		realPlayer, ok := json_map["name"].(string)
-		if !ok {
-			fmt.Println("No name")
-			return c.String(400, "Bad Request: No name")
-		}
-		playerName := strings.TrimSpace(realPlayer)
-		playerName = strings.ToLower(playerName)
+		playerName := cleanName(realPlayer)
 
-		amount, ok := json_map["amount"].(string)
-		if !ok {
-			fmt.Println("No amount")
-			return c.String(400, "Bad Request: No amount")
-		}
-		amountInt, err := strconv.ParseInt(amount, 10, 64)
-		if err != nil {
-			fmt.Println("Error parsing amount")
-			return c.String(400, "Bad Request: Invalid amount")
-		}
 		oldScore, ok := playerScores[playerName]
 		if !ok {
 			fmt.Println("Player not found")
@@ -325,10 +256,8 @@ func main() {
 
 		return c.String(200, fmt.Sprintf("%v", playerScores[playerName]))
 	})
-
 	e.DELETE("/delete-player", func(c echo.Context) error {
-		json_map := make(map[string]interface{})
-		err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+		realPlayer, correctPassword, _, err := parseJSON(c)
 		if err != nil {
 			fmt.Println("Error decoding json")
 			return c.String(400, "Bad Request: Invalid JSON")
@@ -338,25 +267,13 @@ func main() {
 		defer Lock.Unlock()
 
 		// verify password
-		inputPassword, ok := json_map["password"].(string)
-		if !ok {
-			fmt.Println("No password")
-			return c.String(400, "Bad Request: No password")
-		}
-		if inputPassword != password {
+		if !correctPassword {
 			fmt.Println("Unauthorized")
 			return c.String(401, "Unauthorized")
 		}
 
-		// verify playername
-		realPlayer, ok := json_map["name"].(string)
-		if !ok {
-			fmt.Println("No name")
-			return c.String(400, "Bad Request: No name")
-		}
-		playerName := strings.TrimSpace(realPlayer)
-		playerName = strings.ToLower(playerName)
 		// verify player exists
+		playerName := cleanName(realPlayer)
 		if _, ok := playerScores[playerName]; !ok {
 			fmt.Println("Player not found")
 			return c.String(400, "Bad Request: Player not found")
@@ -370,6 +287,67 @@ func main() {
 		delete(lastUpdate, playerName)
 
 		return c.String(200, "Player deleted")
+	})
+
+	// game information
+	e.POST("/leaderboard", func(c echo.Context) error {
+		Lock.Lock()
+		defer Lock.Unlock()
+
+		// list of all players and their scores
+		playerWithScores := make([][]string, 0)
+		for playerName, score := range playerScores {
+			playerWithScores = append(playerWithScores, []string{playerNames[playerName], fmt.Sprintf("%d", score)})
+		}
+
+		// sort players by score, then by last update time
+		sort.Slice(playerWithScores, func(i, j int) bool {
+			a := playerScores[playerWithScores[i][0]]
+			b := playerScores[playerWithScores[j][0]]
+			if a == b {
+				return lastUpdate[playerWithScores[i][0]].Before(lastUpdate[playerWithScores[j][0]])
+			}
+			return a > b
+		})
+
+		return c.JSON(200, playerWithScores)
+	})
+	e.POST("/buzzed-in", func(c echo.Context) error {
+		Lock.Lock()
+		defer Lock.Unlock()
+
+		// list all players and their buzz in times, in order of buzz in
+		players := make([][]string, 0)
+		for playerName, _ := range playerTimes {
+			players = append(players, []string{playerNames[playerName], playerTimes[playerName].Format("03:04:05.000 PM")})
+		}
+		sort.Slice(players, func(i, j int) bool {
+			return playerTimes[players[i][0]].Before(playerTimes[players[j][0]])
+		})
+
+		return c.JSON(200, players)
+	})
+	e.POST("/stats", func(c echo.Context) error {
+		Lock.Lock()
+		defer Lock.Unlock()
+
+		// list all players and their scores and correct answers
+		players := make([][]string, 0)
+		for playerName, score := range playerScores {
+			players = append(players, []string{playerNames[playerName], fmt.Sprintf("%d", score), strings.Trim(strings.Join(playerCorrect[playerName], ","), "[]")})
+		}
+
+		// sort players by score, then by last update time
+		sort.Slice(players, func(i, j int) bool {
+			a := playerScores[players[i][0]]
+			b := playerScores[players[j][0]]
+			if a == b {
+				return lastUpdate[players[i][0]].Before(lastUpdate[players[j][0]])
+			}
+			return a > b
+		})
+
+		return c.JSON(200, players)
 	})
 
 	e.RouteNotFound("/*", controllers.NotFound)
