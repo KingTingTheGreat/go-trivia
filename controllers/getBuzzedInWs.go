c9faa6d3502467532cb25fab1fa9ba40d8ecc487
KingTingTheGreat <jting@bu.edu>
2024-03-09 00:26:22 -0500

    can now handle multiple socket connections

--- controllers/getBuzzedInWs.go
+++ controllers/getBuzzedInWs.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"go-trivia/shared"
 	"go-trivia/utils"
+	"sync"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
+
+var buzzedInConnections = make(map[*websocket.Conn]bool)
+var buzzedInLock = &sync.Mutex{}
 
 func GetBuzzedInWs(c echo.Context) error {
 	conn, err := shared.Upgrader.Upgrade(c.Response(), c.Request(), nil)
@@ -14,7 +19,16 @@ func GetBuzzedInWs(c echo.Context) error {
 		fmt.Println("Error upgrading to buzzed-in websocket")
 		return err
 	}
-	defer conn.Close()
+	defer func(conn *websocket.Conn) {
+		buzzedInLock.Lock()
+		defer buzzedInLock.Unlock()
+		delete(buzzedInConnections, conn)
+		conn.Close()
+	}(conn)
+
+	buzzedInLock.Lock()
+	buzzedInConnections[conn] = true
+	buzzedInLock.Unlock()
 
 	// send initial list of players
 	fmt.Println("sending initial buzzed in")
@@ -23,8 +37,17 @@ func GetBuzzedInWs(c echo.Context) error {
 	// send when new data is available
 	for _ = range shared.BuzzChan {
 		fmt.Println("BuzzChan update")
-		go utils.SendBuzzedIn(conn)
+		go broadcastBuzzedIn()
 	}
 
 	return nil
+}
+
+func broadcastBuzzedIn() {
+	buzzedInLock.Lock()
+	defer buzzedInLock.Unlock()
+
+	for conn, _ := range buzzedInConnections {
+		go utils.SendBuzzedIn(conn)
+	}
 }
--- controllers/getLeaderboardWs.go
+++ controllers/getLeaderboardWs.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"go-trivia/shared"
 	"go-trivia/utils"
+	"sync"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
+
+var leaderboardConnections = make(map[*websocket.Conn]bool)
+var leaderboardLock = &sync.Mutex{}
 
 func GetLeaderboardWs(c echo.Context) error {
 	conn, err := shared.Upgrader.Upgrade(c.Response(), c.Request(), nil)
@@ -14,7 +19,16 @@ func GetLeaderboardWs(c echo.Context) error {
 		fmt.Println("Error upgrading to leaderboard websocket")
 		return err
 	}
-	defer conn.Close()
+	defer func(conn *websocket.Conn) {
+		leaderboardLock.Lock()
+		defer leaderboardLock.Unlock()
+		delete(leaderboardConnections, conn)
+		conn.Close()
+	}(conn)
+
+	leaderboardLock.Lock()
+	leaderboardConnections[conn] = true
+	leaderboardLock.Unlock()
 
 	// send initial list of players
 	fmt.Println("sending initial leaderboard")
@@ -23,8 +37,17 @@ func GetLeaderboardWs(c echo.Context) error {
 	// send when new data is available
 	for _ = range shared.LeaderboardChan {
 		fmt.Println("LeaderboardChan update")
-		go utils.SendLeaderboard(conn)
+		go broadcastLeaderboard()
 	}
 
 	return nil
+}
+
+func broadcastLeaderboard() {
+	leaderboardLock.Lock()
+	defer leaderboardLock.Unlock()
+
+	for conn, _ := range leaderboardConnections {
+		go utils.SendLeaderboard(conn)
+	}
 }
